6d5d45c248f58501c4eca974306b51fdcd5941ba
hiro-engineer4 <thejunkcocoa@gmail.com>
2025-05-06 23:46:44 +0900

    pwa化

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = withPWA(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ReactNode } from 'react';
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { ReactNode } from 'react'
 
 const theme = createTheme({
   palette: {
@@ -10,13 +10,22 @@ const theme = createTheme({
       main: '#1976d2',
     },
   },
-});
+})
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <head>
         <title>割り勘計算ツール</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192.png" />
+        <meta name="theme-color" content="#EB6100" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </head>
       <body>
         <ThemeProvider theme={theme}>
@@ -25,5 +34,5 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
